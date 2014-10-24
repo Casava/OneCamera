d8a82dedbbd065d49a273cc7e4ef245303b22af1
comiscience <comiscience@hotmail.fr>
2014-10-24 15:36:21 +0200

    update

--- OneCamera/OneCamera/camera.js
+++ OneCamera/OneCamera/camera.js
@@ -3,48 +3,95 @@
 
 //http://www.html5rocks.com/en/tutorials/webrtc/basics/
 
-    function hasGetUserMedia() {
-        return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                  navigator.mozGetUserMedia || navigator.msGetUserMedia);
+//https://github.com/burtlo/videojs/blob/master/views/index.erb
+
+var rafId;
+
+var recordingFrames = [];
+
+var cameraRecordCanvas = document.createElement('canvas');
+cameraRecordCanvas.width = 640;
+cameraRecordCanvas.height = 360;
+var ctx = cameraRecordCanvas.getContext('2d');
+
+var cameraVideo = document.getElementById('videoCamera');;
+
+var localCameraMediaStream;
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+function drawVideoFrame(time) {
+    rafId = window.requestAnimationFrame(drawVideoFrame);
+    ctx.drawImage(video, 0, 0, cameraRecordCanvas.width, cameraRecordCanvas.height);
+    //frames.push(cameraRecordCanvas.toDataURL('image/webp', 1));
+    var image = cameraRecordCanvas.toDataURL("image/webp", 1);
+    //ws.send(dataUritoView(image));
+    console.log(dataUritoView(image));
+};
+
+
+function dataUritoView(dataUri) {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    var byteString = atob(dataUri.split(',')[1]);
+
+    var ab = new ArrayBuffer(byteString.length);
+    var view = new DataView(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        view.setUint8(i, byteString.charCodeAt(i), true);
     }
+    return view;
+}
 
-    var vgaConstraints = {
-        video: {
-            mandatory: {
-                maxWidth: 640,
-                maxHeight: 360
-            }
-        },
-        video: true,
-        audio: false
-    };
+cameraVideo.addEventListener('loadeddata', function () {
 
-    var errorCallback = function (e) {
-        console.log('Reeeejected!', e);
-    };
+    rafId = window.requestAnimationFrame(drawVideoFrame);
+    ctx.drawImage(video, 0, 0, cameraRecordCanvas.width, cameraRecordCanvas.height);
+    console.log("Saving Frame");
+    //frames.push(cameraRecordCanvas.toDataURL('image/webp', 1));
+    var image = cameraRecordCanvas.toDataURL("image/webp", 1);
+    //ws.send(dataUritoView(image));
+    console.log(dataUritoView(image));
+});
 
-    function startCamera() {
-        navigator.getUserMedia = navigator.getUserMedia ||
-                      navigator.webkitGetUserMedia ||
-                      navigator.mozGetUserMedia ||
-                      navigator.msGetUserMedia;
+function startCamera() {
+    navigator.getUserMedia = navigator.getUserMedia ||
+                    navigator.webkitGetUserMedia ||
+                    navigator.mozGetUserMedia ||
+                    navigator.msGetUserMedia;
 
-        // Not showing vendor prefixes.
-        navigator.getUserMedia(vgaConstraints, function (localMediaStream) {
-            var video = document.getElementById('videoCamera');
-            video.src = window.URL.createObjectURL(localMediaStream);
-
-            // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
-            // See crbug.com/110938.
-            video.onloadedmetadata = function (e) {
-                // Ready to go. Do some stuff.
-            };
-        }, errorCallback);
-    }
-
-    $('#btnCamera').click(function () {
-        if(hasGetUserMedia())
+    // Not showing vendor prefixes.
+    navigator.getUserMedia(
         {
-            startCamera();
+            video: {
+                mandatory: {
+                    maxWidth: 640,
+                    maxHeight: 360
+                }
+            },
+            audio: false
+        }, 
+        function (localMediaStream) {
+            cameraVideo.src = window.URL.createObjectURL(localMediaStream);
+            localCameraMediaStream = localMediaStream;
+        }, 
+        function (e) {
+            console.log('Reeeejected!', e);
         }
-    });
+    );
+}
+
+function hasGetUserMedia() {
+    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+$('#btnCamera').click(function () {
+    if(hasGetUserMedia())
+    {
+        startCamera();
+    }
+});
+
+
