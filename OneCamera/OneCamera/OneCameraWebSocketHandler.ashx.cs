031e11f291aab5c929d39210fea5fdb495685b79
comiscience <comiscience@hotmail.fr>
2014-10-25 00:13:15 +0200

    update

--- OneCamera/OneCamera/OneCameraWebSocketHandler.ashx.cs
+++ OneCamera/OneCamera/OneCameraWebSocketHandler.ashx.cs
@@ -30,25 +30,26 @@ namespace OneCamera
             WebSocket socket = context.WebSocket;
             while (true)
             {
-                var buffer = new ArraySegment<byte>(new byte[1024]);
+                var buffer = new ArraySegment<byte>(new byte[10240]);
                 WebSocketReceiveResult result = await socket.ReceiveAsync(buffer, CancellationToken.None);
                 if (socket.State == WebSocketState.Open)
                 {
-                    var videoLength = VideoData.Instance.GetVideoLength();
+                    //var videoLength = VideoData.Instance.GetVideoLength();
 
-                    //string userMessage = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+                    string userMessage = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
                     //userMessage = string.Format("From OneCamera, You sent: {0} at {1}; Video size : {2} Bytes",
                     //    userMessage, DateTime.Now.ToLongTimeString(), videoLength);
-                    //buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(userMessage));
-                    //await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                    buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(userMessage));
+                    if (buffer.Count > 5)
+                        await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
 
-                    for (int i = 0; i < videoLength; i += 5012)
-                    {
-                        var bytes = VideoData.Instance.GetVideoBytes(i, 5012);
-                        VideoData.Instance.GetVideoBytes(i, (int) videoLength - i);
-                        var videoBuffer = new ArraySegment<byte>(bytes);
-                        await socket.SendAsync(videoBuffer, WebSocketMessageType.Binary, true, CancellationToken.None);
-                    }
+                    //for (int i = 0; i < videoLength; i += 5012)
+                    //{
+                    //    var bytes = VideoData.Instance.GetVideoBytes(i, 5012);
+                    //    VideoData.Instance.GetVideoBytes(i, (int) videoLength - i);
+                    //    var videoBuffer = new ArraySegment<byte>(bytes);
+                    //    await socket.SendAsync(videoBuffer, WebSocketMessageType.Binary, true, CancellationToken.None);
+                    //}
                 }
                 else
                 {
--- OneCamera/OneCamera/camera.js
+++ OneCamera/OneCamera/camera.js
@@ -6,91 +6,96 @@
 //https://github.com/burtlo/videojs/blob/master/views/index.erb
 
 var rafId;
-
-var recordingFrames = [];
-
-var cameraRecordCanvas = document.createElement('canvas');
-cameraRecordCanvas.width = 640;
-cameraRecordCanvas.height = 360;
-var ctx = cameraRecordCanvas.getContext('2d');
-
-var cameraVideo = document.getElementById('videoCamera');;
-
+var cameraVideo;
 var localCameraMediaStream;
 
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                              window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-
-function drawVideoFrame(time) {
-    rafId = window.requestAnimationFrame(drawVideoFrame);
-    ctx.drawImage(video, 0, 0, cameraRecordCanvas.width, cameraRecordCanvas.height);
-    //frames.push(cameraRecordCanvas.toDataURL('image/webp', 1));
-    var image = cameraRecordCanvas.toDataURL("image/webp", 1);
-    console.log(image);
-    //ws.send(dataUritoView(image));
-};
+$().ready(function () {
+    var cameraRecordCanvas = document.createElement('canvas');
+    cameraRecordCanvas.width = 640;
+    cameraRecordCanvas.height = 360;
+    var ctx = cameraRecordCanvas.getContext('2d');
 
 
-function dataUritoView(dataUri) {
-    // convert base64 to raw binary data held in a string
-    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-    var byteString = atob(dataUri.split(',')[1]);
+    window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                                  window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
-    var ab = new ArrayBuffer(byteString.length);
-    var view = new DataView(ab);
-    for (var i = 0; i < byteString.length; i++) {
-        view.setUint8(i, byteString.charCodeAt(i), true);
-    }
-    return view;
-}
+    function drawVideoFrame(time) {
+        rafId = window.requestAnimationFrame(drawVideoFrame);
+        ctx.drawImage(cameraVideo, 0, 0, cameraRecordCanvas.width, cameraRecordCanvas.height);
+        //frames.push(cameraRecordCanvas.toDataURL('image/webp', 1));
+        var image = cameraRecordCanvas.toDataURL("image/webp", 0.5);
+        //console.log(image);
+        ws.send(image);
+        //dataUritoView(image);
+        //ws.send(dataUritoView(image));
+    };
 
-cameraVideo.addEventListener('loadeddata', function () {
 
-    rafId = window.requestAnimationFrame(drawVideoFrame);
-    ctx.drawImage(video, 0, 0, cameraRecordCanvas.width, cameraRecordCanvas.height);
-    //frames.push(cameraRecordCanvas.toDataURL('image/webp', 1));
-    var image = cameraRecordCanvas.toDataURL("image/webp", 1);
-    console.log(image);
-    //ws.send(dataUritoView(image));
-});
-
-function startCamera() {
-    navigator.getUserMedia = navigator.getUserMedia ||
-                    navigator.webkitGetUserMedia ||
-                    navigator.mozGetUserMedia ||
-                    navigator.msGetUserMedia;
-
-    // Not showing vendor prefixes.
-    navigator.getUserMedia(
-        {
-            video: {
-                mandatory: {
-                    maxWidth: 640,
-                    maxHeight: 360
-                }
-            },
-            audio: false
-        }, 
-        function (localMediaStream) {
-            cameraVideo.src = window.URL.createObjectURL(localMediaStream);
-            localCameraMediaStream = localMediaStream;
-        }, 
-        function (e) {
-            console.log('Reeeejected!', e);
+    function dataUritoView(dataUri) {
+        // convert base64 to raw binary data held in a string
+        // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+        console.log('dataUri ' + dataUri.length);
+        var byteString = atob(dataUri.split(',')[1]);
+        console.log('byteString ' + byteString.length);
+        var ab = new ArrayBuffer(byteString.length);
+        var view = new DataView(ab);
+        for (var i = 0; i < byteString.length; i++) {
+            view.setUint8(i, byteString.charCodeAt(i), true);
         }
-    );
-}
-
-function hasGetUserMedia() {
-    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                navigator.mozGetUserMedia || navigator.msGetUserMedia);
-}
-
-$('#btnCamera').click(function () {
-    if(hasGetUserMedia())
-    {
-        startCamera();
+        //console.log('view ' + view.length);
+        return view;
     }
-});
 
+
+
+    function startCamera() {
+        navigator.getUserMedia = navigator.getUserMedia ||
+                        navigator.webkitGetUserMedia ||
+                        navigator.mozGetUserMedia ||
+                        navigator.msGetUserMedia;
+
+        // Not showing vendor prefixes.
+        navigator.getUserMedia(
+            {
+                video: {
+                    mandatory: {
+                        maxWidth: 640,
+                        maxHeight: 360
+                    }
+                },
+                audio: false
+            },
+            function (localMediaStream) {
+                cameraVideo = document.getElementById('videoCamera');
+                cameraVideo.src = window.URL.createObjectURL(localMediaStream);
+                localCameraMediaStream = localMediaStream;
+                cameraVideo.addEventListener('loadeddata', function () {
+                    rafId = window.requestAnimationFrame(drawVideoFrame);
+                    ctx.drawImage(cameraVideo, 0, 0, cameraRecordCanvas.width, cameraRecordCanvas.height);
+                    //frames.push(cameraRecordCanvas.toDataURL('image/webp', 1));
+                    var image = cameraRecordCanvas.toDataURL("image/webp", 1);
+                    console.log(image);
+                    //ws.send(dataUritoView(image));
+                });
+            },
+            function (e) {
+                console.log('Reeeejected!', e);
+            }
+        );
+    };
+
+    function hasGetUserMedia() {
+        return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                    navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    }
+
+    $('#btnCamera').click(function () {
+        console.log("Clicked");
+        if (hasGetUserMedia()) {
+            console.log("has Get User Media");
+            startCamera();
+        }
+    });
+
+});
 
