b77a3c85392aa3d138f3a3a3f2902b5ded83fb50
sawyerlin <sawyer.lin@gmail.com>
2014-10-26 16:43:55 +0100

    Add Sender and recevier

---
+++ OneCamera/OneCamera/Scripts/recevier.js
@@ -0,0 +1,42 @@
+$(function(){
+
+	var socket;
+
+	var recevier = $("#recevier");
+	// recevier.attr("src", evt.data);
+	recevier.css("width", 500);
+	recevier.css("height", 250);
+	recevier.css("background-color", "#000000");
+
+	$('#start').on('click', function(){
+		console.log("started");
+		// socket = new WebSocket("ws://onecamera.azurewebsites.net/OneCamera/OneCameraWebSocketHandler.ashx");
+		socket = new WebSocket("ws://localhost:8080", 'echo-portocol');
+
+		socket.onopen = function(){
+			console.log("connected");
+		}
+
+		socket.onmessage = function(){
+			
+		}
+
+		socket.onerror = function(env){
+			console.log(env.message);
+		}
+
+		socket.onclose = function(){
+			console.log("disconnected");
+		}
+
+		console.log("Start Receving");
+	});
+
+	$('#close').on('click', function(){
+		if(socket != undefined && socket.readyState == WebSocket.OPEN)
+		{
+			console.log('Close Receving');
+			socket.close();
+		}
+	});
+});
---
+++ OneCamera/OneCamera/Scripts/sender.js
@@ -0,0 +1,68 @@
+$(function($){
+
+	var ws = new WebSocket("ws://localhost:8080", 'echoi-portocol');
+
+	var cameraRecordCanvas = $('#videocanvas');
+
+	window.requestAnimationFrame = window.requestAnimationFrame
+				|| window.mozRequestAnimationFrame
+				|| window.webkitRequestAnimationFrame
+				|| window.msRequestAnimationFrame;
+
+	function drawVideoFrame(time){
+		rafId = window.requestAnimationFrame(drawVideoFrame);
+
+		var ctx = cameraRecordCanavas.getContext('2d');
+		ctx.drawImage(cameraVideo, 0, 0, 500, 250);
+		var image = cameraRecordCanvas.toDataURL("image/webp", 0.5);
+		ws.send(image);
+	}	
+	
+
+	function startCamera(){
+		 navigator.getUserMedia = navigator.getUserMedia ||
+                        navigator.webkitGetUserMedia ||
+                        navigator.mozGetUserMedia ||
+                        navigator.msGetUserMedia;
+
+		// Not showing vendor prefixes.
+		navigator.getUserMedia(
+		    {
+			video: {
+			    mandatory: {
+				maxWidth: 500,
+				maxHeight: 250
+			    }
+			},
+			audio: false
+		    },
+		    function (localMediaStream) {
+			cameraVideo = document.getElementById('videoCamera');
+			cameraVideo.src = window.URL.createObjectURL(localMediaStream);
+			localCameraMediaStream = localMediaStream;
+			cameraVideo.addEventListener('loadeddata', function () {
+			    rafId = window.requestAnimationFrame(drawVideoFrame);
+			});
+		    },
+		    function (e) {
+			console.log('Reeeejected!', e);
+		    }
+		);
+
+
+	}
+
+	function hasGetUserMedia(){
+		return !!(navigator.getUserMedia || navigator.webkitGetUserMedia
+			|| navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+	}
+
+	$('#start').on('click', function(){
+		if(hasGetUserMedia()){
+			console.log('Has user media');
+			startCamera();
+		}
+	});
+
+})(jQuery);
